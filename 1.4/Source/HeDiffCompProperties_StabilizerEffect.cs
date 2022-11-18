410d3687e3d04d56342aefd685bd4ee9c1fae536
pizzapolice <maokevin@gmail.com>
2022-11-18 08:09:53 -0700

    Change surgery to go to torso

--- 1.4/Source/HeDiffCompProperties_StabilizerEffect.cs
+++ 1.4/Source/HeDiffCompProperties_StabilizerEffect.cs
@@ -1,26 +1,10 @@
 using RimWorld;
 using Verse;
 
+
+namespace AnimalStabilizer;
 public class HediffCompProperties_StabilizerEffect : HediffCompProperties
 {
-	public FleckDef fleck;
-
-	public ThingDef mote;
-
-	public int moteCount = 3;
-
-	public FloatRange moteOffsetRange = new FloatRange(0.2f, 0.4f);
-
-	public ThingDef filth;
-
-	public int filthCount = 4;
-
-	public HediffDef injuryCreatedOnDeath;
-
-	public IntRange injuryCount;
-
-	public SoundDef sound;
-
 	public HediffCompProperties_StabilizerEffect()
 	{
 		compClass = typeof(HediffComp_StabilizerEffect);
