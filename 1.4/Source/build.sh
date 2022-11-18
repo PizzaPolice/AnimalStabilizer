8de887bb132629471762e1449eb759423f684325
pizzapolice <maokevin@gmail.com>
2022-11-18 08:50:48 -0700

    Added build sh to auto go to assembles

--- 1.4/Source/build.sh
+++ 1.4/Source/build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.7.2-api/ dotnet build AnimalStabilizer.csproj /property:Configuration=Release
+
+cp ./bin/Release/net4.7.2/AnimalStabilizer.dll ../Assemblies
